22f803a550dd23218061532bb310509306eb7899
kumardeepak99 <dkumarjha999@gmail.com>
2023-09-26 00:13:14 +0530

    BFF layer with TDD

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
-import "@testing-library/jest-dom/extend-expect";
+require("@testing-library/jest-dom/extend-expect");
 
 jest.mock("next/navigation", () => require("next-router-mock"));
 
---
+++ src/app/constants/Api.ts
@@ -0,0 +1,9 @@
+export const ApiConstants = {
+  Urls: {
+    GetSchoolDistricts: process.env.NEXT_PUBLIC_SCHOOL_DISTRICT_BASE_URL + "/SchoolDistrict",
+  },
+  Response: {
+    SUCCESS: "Ok",
+    ERROR: "Something went wrong while processing",
+  },
+};
